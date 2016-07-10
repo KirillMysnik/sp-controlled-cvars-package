93400f43a8dbfaf758bf2ac804ba4f360f938ca8
Kirill Mysnik <kirill@mysnik.com>
2016-07-10 16:23:29 +0300

    Updated sound_handler and sound_nullable_handlers to be able to use StreamSound

--- addons/source-python/packages/custom/controlled_cvars/handlers.py
+++ addons/source-python/packages/custom/controlled_cvars/handlers.py
@@ -1,5 +1,6 @@
 from colors import Color
-from engines.sound import Sound
+from core import GAME_NAME
+from engines.sound import Sound, StreamSound
 
 from . import InvalidValue
 
@@ -37,13 +38,32 @@ def list_handler(cvar):
     return cvar.get_string().strip(',').split(',')
 
 
+def _find_best_sound_class(sample):
+    if GAME_NAME not in ("csgo",):
+        return Sound
+
+    sample = sample.lower()
+
+    if sample.endswith(".wav"):  # StreamSound won't work for wav files anyways
+        return Sound
+
+    if sample.startswith("music/"):  # No need for StreamSound
+        return Sound
+
+    return StreamSound
+
+
 def sound_handler(cvar):
-    return Sound(cvar.get_string())
+    sample = cvar.get_string()
+    return _find_best_sound_class(sample)(sample)
 
 
 def sound_nullable_handler(cvar):
-    val = cvar.get_string()
-    return None if val == "" else Sound(val)
+    sample = cvar.get_string()
+    if not sample:
+        return None
+
+    return _find_best_sound_class(sample)(sample)
 
 
 def string_handler(cvar):
